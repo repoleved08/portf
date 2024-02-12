0ea7cdcf0a7afaa33affb8bfb8d2f8f1c239a713
repoleved08 <geekbii08@gmail.com>
2024-02-12 20:46:53 +0300

    nav change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            NormanGeek1
           </a>
         </div>
         
